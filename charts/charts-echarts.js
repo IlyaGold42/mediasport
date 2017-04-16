7b75bb1588907b2107ad80fa4651efac5020e3af
IlyaGold42 <goldenberg.ilya228@yandex.ru>
2017-04-16 16:39:08 +0300

    Added leagues charts

--- charts/charts-echarts.js
+++ charts/charts-echarts.js
@@ -81,6 +81,59 @@ jQuery(document).ready(function() {
                     data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
                 }]
             });
+        
+            //--- BAR ---
+            var myChart2 = ec.init(document.getElementById('echarts_leagues'));
+            myChart2.setOption({
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['Лига 1', 'Лига 2']
+                },
+                toolbox: {
+                    show: true,
+                    feature: {
+                        mark: {
+                            show: true
+                        },
+                        dataView: {
+                            show: true,
+                            readOnly: false
+                        },
+                        magicType: {
+                            show: true,
+                            type: ['line', 'bar']
+                        },
+                        restore: {
+                            show: true
+                        },
+                        saveAsImage: {
+                            show: true
+                        }
+                    }
+                },
+                calculable: true,
+                xAxis: [{
+                    type: 'category',
+                    data: ['голы', 'передач', 'гол+пас', 'фол+', 'фол-', 'др+', 'др-', 'пот', 'пер', 'ств', 'мимо', 'min']
+                }],
+                yAxis: [{
+                    type: 'value',
+                    splitArea: {
+                        show: true
+                    }
+                }],
+                series: [{
+                    name: 'Лига 1',
+                    type: 'bar',
+                    data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+                }, {
+                    name: 'Лига 2',
+                    type: 'bar',
+                    data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+                }]
+            });
         }
     );
 });
